9e5d4914783e72a081dd549b581f6a0297cb28e8
CaylaT <72427853+caylaet@users.noreply.github.com>
2021-09-16 10:49:26 -0400

    It is working!
    After initial push, I redid my javascript and opened in browser to make sure it was working and it is!

--- main.js
+++ main.js
@@ -2,12 +2,28 @@ const links = [
     {
       label: "Week1 notes",
       url: "week1/index.html"
-    }
+    },
+    {
+        label: "Week2 notes",
+        url: "week2/index.html"
+    },
+      {
+        label: "Week3 notes",
+        url: "week3/index.html"
+      }
   ];
   let text;
-  fruits.forEach(myFunction);
+  links.forEach(myFunction);
   
   function myFunction(value) {
-    text += '<li> <a href="'+ value.url + '">'+value.label+ '</a></li>';
+    var label = document.createElement("li");
+    var url = document.createElement("a");
+    var link = document.createTextNode(value.label);         
+    // Append the text node to anchor element.
+    url.appendChild(link); 
+    url.href = value.url
+    label.appendChild(url);
+    document.getElementById("table_of_contents").appendChild(label);
+
   }
-  document.getElementById("table_of_contents").appendChild(text)
+  
