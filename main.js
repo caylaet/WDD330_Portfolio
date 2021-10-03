5e41985fb9a2025960fc940d73000f0ebdbfc226
CaylaT <72427853+caylaet@users.noreply.github.com>
2021-10-02 22:42:44 -0400

    week 3 link
    Forgot to add link to week 3

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ const links = [
     {
       label: "Week2",
       url: "week2/index.html"
-    }
+    },
+    {
+      label: "Week3",
+      url: "week3/index.html"
+    },
   ];
   let text;
   links.forEach(myFunction);
