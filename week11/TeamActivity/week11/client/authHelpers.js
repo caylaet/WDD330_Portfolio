f6d7bf26df5681abc301ba41bc23e0204af2b4df
CaylaT <72427853+caylaet@users.noreply.github.com>
2021-11-26 00:51:16 -0500

    week 11
    Updated Team Activity

--- week11/TeamActivity/week11/client/authHelpers.js
+++ week11/TeamActivity/week11/client/authHelpers.js
@@ -1,3 +1,38 @@
+// Error-Handling Class
+export class ErrorHandler {
+  // Constructor
+  constructor(errorId) {
+    // Get the error ID from week11.js
+    this.errorElement = document.getElementById(errorId);
+  }
+  // Method for handling errors
+  handleError(error, callback) {
+    // Parse error code from error string object
+    const eCode = error.message.substring(0, 3);
+    this.displayError(error);
+    // If login authentication fails, display login form again
+    if (eCode == 401 || eCode == 500) {
+      callback();
+    }
+    // Display the error code to the console
+    console.log(eCode);
+  }
+  // Method for displaying errors
+  displayError(error) {
+    // Display the error message
+    this.errorElement.innerHTML = error.message;
+    // Remove any 'hidden' CSS classes from the error section
+    this.errorElement.classList.remove('hidden');
+  }
+  // Method for clearing any existent errors
+  clearError() {
+    // Clear error field of any existing errors
+    this.errorElement.innerHTML = '';
+    // Add CSS 'hidden' classes to the error section
+    this.errorElement.classList.add('hidden');
+  }
+}
+
 // Server Address
 const baseURL = 'http://127.0.0.1:3000/';
 // helper function to make an http request with fetch.
@@ -14,10 +49,12 @@ export async function makeRequest(url, method = 'GET', body = null, token = null
   if (method == 'POST' || method == 'PUT') {
     options.body = JSON.stringify(body);
   }
+
   // if a token was passed in we should send it on.
   if (token) {
     options.headers.Authorization = `Bearer ${token}`;
   }
+  
   const response = await fetch(baseURL + url, options);
   // in this case we are processing the response as JSON before we check the status. The API we are using will send back more meaningful error messages than the default messages in the response, but we have to convert it before we can get to them.
   const data = await response.json();
@@ -27,7 +64,8 @@ export async function makeRequest(url, method = 'GET', body = null, token = null
 
     console.log(response);
     throw new Error(`${data.status}: ${data.message}`);
-  } else return data;
-
+  } else {
+    return data;
+  }
   // not catching the error here...so we will need to catch it later on and handle it.
 }
--- week11/TeamActivity/week11/client/week11.js
+++ week11/TeamActivity/week11/client/week11.js
@@ -1,12 +1,95 @@
-import { makeRequest } from "./authHelpers.js";
-import Auth from "./auth.js";
+import { ErrorHandler, makeRequest } from './authHelpers.js';
+import Auth from './auth.js';
 
-makeRequest('login', 'POST', {
-    password: 'user1',
-    email: 'user1@email.com'
-    });
+// makeRequest('login', 'POST', {
+//     password: 'user1',
+//     email: 'user1@email.com'
+// });
 
-let submit =document.getElementById('submit');
+// Create instances
+const myEInstance = new ErrorHandler('errors');
+var myInstance = new Auth(myEInstance);
 
-let myInsantce = new Auth();
-submit.addEventListener("click",myInsantce.login);
+// Create event listener
+var submit = document.getElementById('submit');
+submit.addEventListener('click', function() {
+    // Call the getPosts method on successful login
+    myInstance.login(getPosts);
+});
+
+// Create a function to pull all of the posts from the API and call it after a successful login
+async function getPosts() {
+    try {
+        // Initialize a constant response object
+        const response = await makeRequest('posts', 'GET', null, myInstance.token);
+        // Make sure the form is shown
+        removeClass();
+        // Display the response
+        console.log(response);
+        // Display a list of posts
+        var list = document.getElementById('list');
+        // Set list string to empty
+        list.innerHTML = '';
+        // Loop through the responses
+        for (var i = 0; i < response.length; i++) {
+            // Create a new list item
+            var item = document.createElement('li');
+            // Add each response piece to the list item
+            item.appendChild(document.createTextNode(response[i].title + ":" + response[i].content));
+            // Add the item to the list
+            list.appendChild(item);
+        }
+        // Clear any possible errors
+        myEInstance.clearError();
+    } catch (error) {
+        // Display any possible errors
+        console.log(error);
+        myEInstance.handleError(error);
+    }
+}
+
+// Create a listener for handling a new post submission event
+var create = document.getElementById('createSubmit');
+create.addEventListener('click', createPost())
+
+// Method for handling new post creation events
+async function createPost() {
+    // Call the form named postForm
+    const form = document.forms.postForm;
+    // Display the form to the console as a table
+    console.dir(form);
+    // Check form validity
+    if (form.title.validity.valid && form.content.validity.valid) {
+        // Clear any possible errors
+        myEInstance.clearError();
+        // Set new form data object
+        const data = {
+            title: form.title.value,
+            content: form.content.value
+        };
+        try {
+            // Create a response object
+            const response = await makeRequest('posts', 'POST', data, myInstance.token);
+            // Display the created post to the console
+            console.log('Post create: ', data);
+            // Clear the form title and content values
+            form.title.value = '';
+            form.content.value = '';
+            // Call the getPosts method to display updated post list
+            getPosts();
+        } catch (error) {
+            // Display error message
+            console.log(error);
+            myEInstance.handleError(error);
+        }
+    } else {
+        myEInstance.displayError({ message: 'Title and Content are required' });
+    }
+}
+
+// Function to remove class list items
+function removeClass() {
+    document.getElementById('content').classList.remove('hidden');
+    document.getElementById('postForm').classList.remove('hidden');
+    document.getElementById('listDiv').classList.remove('hidden');
+}
