624933404505843f9d12393192f836eb68f895e6
CaylaT <72427853+caylaet@users.noreply.github.com>
2021-12-05 01:30:26 -0500

    Final Project
    Cleaned up code and started working on application working on mobile device.

--- FinalProject/abc.js
+++ FinalProject/abc.js
@@ -99,19 +99,63 @@ function allowDrop(ev){
     ev.preventDefault();
 }
 start();
-let box = getElement("optionOne")
 
-box.addEventListener('touchmove',function(e) {
+let boxes = document.getElementsByClassName("lowerCaseLetter");
+
+for (let i=0; i< boxes.length; i++){
+    console.log (boxes[i]);
+    boxes[i].addEventListener('touchmove', moveOnTouch);
+    boxes[i].addEventListener('touchend', checkOnEnd);
+};
+
+
+function moveOnTouch(e){
     // grab the location of touch
     var touchLocation = e.targetTouches[0];
-    
+    let lowercaseLetter = e.target;
+        
     // assign box new coordinates based on the touch.
-    box.style.left = touchLocation.pageX + 'px';
-    box.style.top = touchLocation.pageY + 'px';
-  });
+    lowercaseLetter.style.left = touchLocation.pageX + 'px';
+    lowercaseLetter.style.top = touchLocation.pageY + 'px';
+}
 
-box.addEventListener('touchend', function(e) {
+function checkOnEnd(e){
+    let lowercase = e.target ;
     // current box position.
-    var x = parseInt(box.style.left);
-    var y = parseInt(box.style.top);
-  })
+    var x = parseInt(lowercase.style.left);
+    console.log(lowercase.style.left);
+    var y = parseInt(lowercase.style.top);
+    console.log(lowercase.innerHTML);
+    console.log(lowercase.style.top);
+    checkPlacement(x,y);
+}
+
+function checkPlacement(x,y){
+    console.log( x, y)
+
+    if (x > 140 && x < 168){
+        console.log("X made it!");
+
+    }
+    if (y > 30 && y < 65 ){
+        console.log("Y made it!");
+    }
+
+}
+
+// box.addEventListener('touchmove',function(e) {
+//     // grab the location of touch
+//     var touchLocation = e.targetTouches[0];
+    
+//     // assign box new coordinates based on the touch.
+//     box.style.left = touchLocation.pageX + 'px';
+//     box.style.top = touchLocation.pageY + 'px';
+//   });
+
+// box.addEventListener('touchend', function(e) {
+//     // current box position.
+//     var x = parseInt(box.style.left);
+//     var y = parseInt(box.style.top);
+//     let lowercase = e.target ;
+//     console.log(lowercase.innerHTML);
+//   })
